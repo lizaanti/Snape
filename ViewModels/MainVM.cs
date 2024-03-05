c8bedd698a53a3d366b642ac3b8ffe8a56fdc728
Елизавета Антипова <217054@edu.fa.ru>
2024-03-05 17:12:04 +0300

    MainMV.1

---
+++ ViewModels/MainVM.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snape.ViewModels
+{
+    internal class MainVM : BindableBase
+    {
+		private bool _continueGame; //флаг, отвечающий за стоп или продолжение команды
+
+		public bool  ContinueGame
+		{
+			get => _continueGame; 
+			private set 
+			{ 
+				_continueGame = value;
+				RaisePropertyChanged(nameof(ContinueGame)); //метод для уведомления системы об изменении свойства
+			}
+		}
+
+		public DelegateCommand StartStopCommand { get; }
+
+		public MainVM() {
+			StartStopCommand = new DelegateCommand(() => ContinueGame = !ContinueGame);
+		}
+    }
+}
